7460a119d8eefbbd5d763e51dd2d34fd58f9dd39
pabcrudel <pabcrudel@gmail.com>
2023-10-13 14:18:25 +0200

    Added some navigation links

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <NuxtPage/>
 </template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>{{ title }}</h1>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/projects/1">Lorem 1</NuxtLink></li>
+      <li><NuxtLink to="/projects/2">Ipsum 2</NuxtLink></li>
+      <li><NuxtLink to="/projects/3">Dolor 3</NuxtLink></li>
+    </ul>
+  </nav>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium alias
-  tempora quaerat. Consectetur vel consequuntur possimus vero? Repellendus, fuga
-  voluptatem modi aut veritatis sed iste optio libero labore vel, nam nisi
-  mollitia quas dolore eligendi eum qui sunt a id omnis sit et aliquid quia. Sed
-  amet dignissimos itaque accusamus.</p>
-</template>
+    tempora quaerat. Consectetur vel consequuntur possimus vero? Repellendus, fuga
+    voluptatem modi aut veritatis sed iste optio libero labore vel, nam nisi
+    mollitia quas dolore eligendi eum qui sunt a id omnis sit et aliquid quia. Sed
+    amet dignissimos itaque accusamus.</p></template>
 
 <script setup lang="ts">
 const title = "Welcome to my projects";
