6609ee08d0b9af332476bf38c917eb59d9d35f26
pabcrudel <pabcrudel@gmail.com>
2023-10-13 14:10:10 +0200

    Added some example text to fill the website

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,5 +1,10 @@
 <template>
   <h1>Project {{ id }}</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium alias
+  tempora quaerat. Consectetur vel consequuntur possimus vero? Repellendus, fuga
+  voluptatem modi aut veritatis sed iste optio libero labore vel, nam nisi
+  mollitia quas dolore eligendi eum qui sunt a id omnis sit et aliquid quia. Sed
+  amet dignissimos itaque accusamus.</p>
 </template>
 
 <script setup lang="ts">
