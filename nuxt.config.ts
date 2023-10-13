16e66ec9003853f46aa62fde69a79daad5b14bbf
pabcrudel <pabcrudel@gmail.com>
2023-10-13 18:28:16 +0200

    Base URL will defer between environments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+  app: {
+    baseURL: process.env.NODE_ENV === "production" ? "/my-first-nuxt-app/" : "/"
+  }
 })
