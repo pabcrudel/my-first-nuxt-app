a4926631c4b83f840744ee1446c5413fd42f8902
pabcrudel <pabcrudel@gmail.com>
2023-10-14 14:16:16 +0200

    Dynamically render projects

--- layouts/projects.vue
+++ layouts/projects.vue
@@ -11,9 +11,12 @@
         </ul>
       </div>
       <ul class="flex gap-4 justify-end mt-2">
-        <li><NuxtLink to="/projects/1">Lorem 1</NuxtLink></li>
-        <li><NuxtLink to="/projects/2">Ipsum 2</NuxtLink></li>
-        <li><NuxtLink to="/projects/3">Dolor 3</NuxtLink></li>
+        <li
+          v-for="(project, index) in projects"
+          :key="index"
+        >
+          <NuxtLink :to="`/projects/${linkify(project)}`">{{ project }}</NuxtLink>
+        </li>
       </ul>
     </nav>
   </header>
@@ -22,6 +25,18 @@
     <slot />
   </div>
 </template>
+
+<script setup>
+const projects = [
+  "EcoConnect",
+  "TechHarbor",
+  "HealthHub",
+  "ArtVista",
+  "EducationElevate"
+];
+
+const linkify = (title) => title.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+</script>
 
 <style scoped>
 .router-link-exact-active, #parent {
